01cdc39709def62e3ecd0a11b66710eeae1a896e
wtiinfo <wanbrwan@gmail.com>
2021-09-12 21:02:26 -0300

    Iniciando jpa-ds, recordando conceitos

---
+++ src/main/java/com/wtiinfo/app/JpaDsApplication.java
@@ -0,0 +1,13 @@
+package com.wtiinfo.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaDsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaDsApplication.class, args);
+	}
+
+}
